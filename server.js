c9d132972298928d87c97bd1df337675f7ae6896
Bruno <bruno.rp45@gmail.com>
2021-08-25 15:28:25 -0300

    testando segurança da URI

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb%2Bsrv%3A%2F%2FramosUser%3Aptxwcj53DcQwyxda%40cluster0.uyrap.mongodb.net%2Fcadastro%3FretryWrites%3Dtrue%26w%3Dmajority', {
+    // userUnifieldTopology: true,
+    useNewUrlParser: true,
+    // userFindAndModify: true
+}, function (err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('MongoDB CONECTADO com sucesso!');     
+    };
+});
+
 
 const app = express();
 const port = process.env.PORT || 5000;
