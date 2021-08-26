06501c2181e8614c504882cca384fed993604245
Bruno <bruno.rp45@gmail.com>
2021-08-26 14:02:41 -0300

    Adicoinadno Swagger

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const routes = require('./src/routes');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocs = require ('./routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,7 +26,8 @@ mongoose.connect('mongodb+srv://ramosUser:ptxwcj53DcQwyxda@cluster0.uyrap.mongod
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
 app.listen(port, function() {
     console.log(`Server runing on port ${port}`);
-})
+});
--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -34,4 +34,4 @@ module.exports = {
         //     return res.status(500).json(user);
         // }
     }
-}
+};
