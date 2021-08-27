6c3c9e83355d179cdae5dc6f1a8d0ad2f3e8aa1f
Bruno <bruno.rp45@gmail.com>
2021-08-26 22:26:20 -0300

    Preparando pra instalar React

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -31,23 +31,40 @@ module.exports = {
         newUsuario.haveVehicle = haveVehicle;
         newUsuario.haveLisence = haveLisence;
 
-
+        
         newUsuario.save((err, savedUsuario)=> {
             if(err) {
                 console.log(err);
-                return res.status(500).send();
+                return res.status(500).send(err);
             }
             return res.status(200).send(savedUsuario);
-        })
+        });
 
         // let user = Usuario.findOne({email});
         // if(email.unique == true) {
-        //     data = { name, email, gender, cep };
+        //     data = { name, intended, date, matrial, gender, cep, address, number, district, city, state, tel1, tel2, cel, email, indentity, cpf, haveVehicle, haveLisence };
         //     user = await Usuario.create(data);
 
         //     return res.status(200).json(user);
         // } else {
-        //     return res.status(500).json(user);
+        //     return res.status(500).json(err);
         // }
+
+
+
+        // const enviarFormulario = async (req, res) => {
+        //     try {
+        //         if (await Usuario.findOne({ email: req.body.email })) {
+        //             return res.status(400).send({ error: 'Email já cadastrado!' })
+        //         }
+    
+        //         return res.status(201).send({ msg: 'Formulário enviado' });
+        //     } catch (error) {
+        //         return res.status(500).send({ error: 'Erro interno do servidor' });
+        //     }
+        // }
+
+        // enviarFormulario();                     
+    
     }
 };
